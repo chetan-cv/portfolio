d9de84c00ccfd8da7731dce95751b191cddad354
chetan-cv <chetanverma99.cv@gmail.com>
2021-05-15 00:02:59 +0530

    changes in home page and responsive navbar

--- src/components/Header/header.js
+++ src/components/Header/header.js
@@ -1,22 +1,47 @@
 import './header.css';
-import React from 'react';
+import React, { useState } from 'react';
+import { FaBars } from 'react-icons/fa';
+import { IoMdClose } from 'react-icons/io';
 
 const Header = () => {
-    const linkStyle = {
-        textDecoration: "none",
-        color: 'white'
-      };
 
-    return (
-    <header className="App-header">
- <div className="App-header-actions">
-   <a style={linkStyle} href= '#blogs'>Blogs</a>
-   <a style={linkStyle} href= '#projects'>Projects</a>
-   <a style={linkStyle} href= '#about'>About</a>
-   <a style={linkStyle} href= '#home'>Home</a>
- </div>
-</header>
-    );
+  const linkStyle = {
+    textDecoration: "none",
+    color: 'white'
+  };
+
+  const [click, setClick] = useState(false);
+
+  const closeMobileMenu = () => setClick(false);
+
+  const handleClick = () => setClick(!click);
+
+  return (
+      <nav className='navbar'>
+        <div className='navbar-container container'>
+        <a href="#" className='navbar-logo' onClick={closeMobileMenu}>
+              <FaBars className='navbar-icon' />
+              Yo
+            </a>
+          <div className='headerIcons' onClick={handleClick}>
+            {click ? <IoMdClose size={25}/> : < FaBars size={25}/>}
+          </div>
+          <ul className={click ? 'nav-menu active' : 'nav-menu'}>
+            <li className='nav-item'>
+              <a onClick={closeMobileMenu} className='nav-links' href='#home'>Home</a>
+            </li>
+            <li className='nav-item'>
+              <a  onClick={closeMobileMenu} className='nav-links' href='#about'>About</a>
+            </li>
+            <li className='nav-item'>
+              <a onClick={closeMobileMenu} className='nav-links' href='#projects'>Projects</a>
+            </li>
+            <li className='nav-item'>
+              <a onClick={closeMobileMenu}className='nav-links' href='#blogs'>Blogs</a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+  );
 }
 export default Header;
- 
--- src/components/Home/home.js
+++ src/components/Home/home.js
@@ -1,47 +1,103 @@
-import homeBackground from '../../logo/homeBackground.png';
+import flutterLogo from '../../logo/flutterLogo.png';
+import reactLogo from '../../logo/reactLogo.png';
+import htmlLogo from '../../logo/htmlLogo.png';
+import cssLogo from '../../logo/cssLogo.png';
+import pythonLogo from '../../logo/pythonLogo.png';
+import djangoLogo from '../../logo/djangoLogo.png';
+import awsLogo from '../../logo/awsLogo.png';
+import digtialOLogo from '../../logo/digtialOLogo.png';
 import './home.css';
 import Header from '../Header/header';
 import About from '../About/about';
 import Projects from '../Projects/projects';
 import Blogs from '../Blogs/blogs';
-import React, {useState, useEffect} from 'react';
-import {Fade} from 'react-reveal';
+import React, { useState, useEffect } from 'react';
+import { Fade, Slide } from 'react-reveal';
 import Footer from '../Footer/footer';
 
 var jsonData = require("../../data.json");
 
 const Home = () => {
 
-  const [projects,setProjects] = useState([]);
+  const [projects, setProjects] = useState([]);
   const [blogs, setBlogs] = useState([]);
   const [footer, setFooter] = useState([]);
-  
+  const [add, setAdd] = useState(false);
+
   const fetchData = () => {
     setProjects(jsonData.projects);
     setBlogs(jsonData.blogs);
     setFooter(jsonData.footer);
+    setTimeout(() => {
+      setAdd(true);
+    }, 1000);
   }
   useEffect(() => {
     fetchData();
-}, []);
+  }, []);
 
-    return (
+  return (
     <div className="App" id='home'>
 
-     <Header />
-  
+      <Header />
+
       <div className="homeBackground">
-        <img alt="T" src={homeBackground}></img>
-        <div className="homeContent">Hey, I'm <Fade right cascade><span style={{fontSize: '25px', fontWeight:'bolder', color:'#13617d'}}>Chetan Verma</span></Fade><br /><br />A full-stack <Fade left cascade><span style={{fontWeight:'600',color:'#13617d'}}>Developer|Freelancer</span></Fade></div>
+        <div className='intro'>
+          <div className="homeContent">Hey, I'm <Fade right cascade><span style={{ fontSize: '25px', fontWeight: 'bolder', color: '#13617d' }}>Chetan Verma</span></Fade><br /><br />A full-stack <Fade left cascade><span style={{ fontWeight: '600', color: '#13617d' }}>Developer|Freelancer</span></Fade></div>
+        </div>
+
+        {
+          add ? <Slide right duration={1000}><Skills /> </Slide> : null
+        }
+
+
       </div>
-  
-      <About  />
-      <Projects project={projects}/>
-      <Blogs blogs={blogs}/>
-      <Footer footer={footer}/>
+
+      <About />
+      <Projects project={projects} />
+      <Blogs blogs={blogs} />
+      <Footer footer={footer} />
 
     </div>);
 }
 
+const Skills = () => {
+  return (
+    <div className='skills'>
+      <div className='mobile'>
+        <Fade left duration={3000}>
+          <img alt="T" src={flutterLogo}></img>
+        </Fade>
+      </div>
+      <div className='web'>
+        <Fade left duration={3000} delay={600}>
+            <img alt="T" src={htmlLogo}></img>
+        </Fade>
+        <Fade left duration={3000} delay={900}>
+          <img alt="T" src={cssLogo}></img>
+        </Fade>
+        <Fade left duration={3000} delay={1200}>
+          <img alt="T" src={reactLogo}></img>
+        </Fade>
+      </div>
+      <div className='backend'>
+        <Fade left duration={3000} delay={1500}>
+          <img alt="T" src={pythonLogo}></img>
+        </Fade>
+        <Fade left duration={3000} delay={1800}>
+          <img alt="T" src={djangoLogo}></img>
+        </Fade>
+      </div>
+      <div className='cloud'>
+        <Fade left duration={3000} delay={2100}>
+          <img alt="T" src={awsLogo}></img>
+        </Fade>
+        <Fade left duration={3000} delay={2400}>
+          <img alt="T" src={digtialOLogo}></img>
+        </Fade>
+      </div>
+    </div>
+  );
+}
+
 export default Home;
-  
