81edefee428e03a9713dfab1417a04a65cbd4560
chetan-cv <chetanverma99.cv@gmail.com>
2021-05-24 23:29:33 +0530

    Updates

---
+++ precache-manifest.3c8e5ce2f2b66daa5e6615c41de2cb77.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = [
+  {
+    "revision": "68e4a0885b1cb088ec6a",
+    "url": "/portfolio/static/css/main.b6aa115c.chunk.css"
+  },
+  {
+    "revision": "68e4a0885b1cb088ec6a",
+    "url": "/portfolio/static/js/main.ebc5c876.chunk.js"
+  },
+  {
+    "revision": "2cd4594f8650f7e4082a",
+    "url": "/portfolio/static/js/runtime~main.a14f8904.js"
+  },
+  {
+    "revision": "5fef13c3847c1ae6f209",
+    "url": "/portfolio/static/js/2.6b3837f9.chunk.js"
+  },
+  {
+    "revision": "ec86f99e38d92fd4f6f7",
+    "url": "/portfolio/static/js/3.3d2374d6.chunk.js"
+  },
+  {
+    "revision": "cec56b7ad246bc9d5e8d18c92b80bbe9",
+    "url": "/portfolio/static/media/reactLogo.cec56b7a.png"
+  },
+  {
+    "revision": "aec66edfd6dd7c5f35ba2479a78047ac",
+    "url": "/portfolio/static/media/htmlLogo.aec66edf.png"
+  },
+  {
+    "revision": "72b753e35f49cf5d204eadba442df2ff",
+    "url": "/portfolio/static/media/cssLogo.72b753e3.png"
+  },
+  {
+    "revision": "a6766f153130d7d125e5c0fbbb552aad",
+    "url": "/portfolio/static/media/pythonLogo.a6766f15.png"
+  },
+  {
+    "revision": "4dd6a2a30f83a34f258aa5382937266f",
+    "url": "/portfolio/static/media/djangoLogo.4dd6a2a3.png"
+  },
+  {
+    "revision": "3c29dd88c30e5a57ff2029f81abbaf85",
+    "url": "/portfolio/static/media/awsLogo.3c29dd88.png"
+  },
+  {
+    "revision": "c5a80ab8295037d2623a921ec2361798",
+    "url": "/portfolio/static/media/digtialOLogo.c5a80ab8.png"
+  },
+  {
+    "revision": "9f870be65d73893a07c56085a54aa8d8",
+    "url": "/portfolio/static/media/c_logo.9f870be6.svg"
+  },
+  {
+    "revision": "728a1f5839677dcd80634b337ab0eaf9",
+    "url": "/portfolio/static/media/dp.728a1f58.png"
+  },
+  {
+    "revision": "d165a121b1c5d497628911033a0663d7",
+    "url": "/portfolio/static/media/front-removebg-preview.d165a121.png"
+  },
+  {
+    "revision": "25cf3bac3dc5ab5bff0adc14cb15710d",
+    "url": "/portfolio/static/media/back-removebg-preview.25cf3bac.png"
+  },
+  {
+    "revision": "5a4a9b4526623ed116db71a9a2cccf9e",
+    "url": "/portfolio/static/media/uiux-removebg-preview.5a4a9b45.png"
+  },
+  {
+    "revision": "9012c326cd00cbd79ad3f9177c277d96",
+    "url": "/portfolio/static/media/logovfx_v2.9012c326.mp4"
+  },
+  {
+    "revision": "8ed619adb9a94480a0f54c9617e74b3f",
+    "url": "/portfolio/index.html"
+  }
+];
