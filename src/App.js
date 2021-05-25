600ad77a4625eaceef16b8d0fa5fa4db4f33ad7b
chetan-cv <chetanverma99.cv@gmail.com>
2021-05-25 15:43:23 +0530

    updates

--- src/App.js
+++ src/App.js
@@ -1,15 +1,12 @@
 import { BrowserRouter as Router, Route } from "react-router-dom";
-import Home from './components/Home/home';
 import LogoGif from './components/LogoGif/logoGif';
-import React, {useState} from 'react';
+import React from 'react';
 
 function App() {
-  const [A,setA] = useState(false);
-  setInterval(function(){ setA(true) }, 5500);
   return (
     <div>
     <Router basename={process.env.PUBLIC_URL}>
-      <Route exact path="/" component={ A ? Home : LogoGif} />      
+      <Route exact path="/" component={LogoGif} />      
     </Router>
     </div>
   );
--- src/components/LogoGif/logoGif.js
+++ src/components/LogoGif/logoGif.js
@@ -1,18 +1,33 @@
 import React, { useState } from 'react';
 import './logoGif.css';
 import logoVfx from '../../logo/logovfx_v3.mp4';
+import Home from '../Home/home';
 
-const LogoGif = () =>{
-    const [ activateAnimation, setActivateAnimation] = useState(false);
 
-    const endOfVideo = () => setActivateAnimation(true);
+const LogoGif = () => {
+    const [activateAnimation, setActivateAnimation] = useState(false);
+    const [showHome, setShowHome] = useState(false);
+
+    const endOfVideo = () => {
+        setActivateAnimation(true);
+        console.log('end');
+        setInterval(() => {
+            setShowHome(true);
+        }, 800);
+    }
 
     return (
-        <div className={activateAnimation ? 'logoDiagnol' : 'logoGif'}>
-             <video className='video' preload='auto' autoPlay muted height='100%' width='100%' onEnded={endOfVideo} >
-                 <source src={logoVfx} type='video/mp4' />
-             </video>
+        <div>
+            {
+                showHome ? <Home /> : <div className={activateAnimation ? 'logoDiagnol' : 'logoGif'}>
+                    <video className='video' preload='auto' autoPlay muted height='100%' width='100%' onEnded={endOfVideo} >
+                        <source src={logoVfx} type='video/mp4' />
+                    </video>
+                </div>
+            }
+
         </div>
+
     )
 }
 
