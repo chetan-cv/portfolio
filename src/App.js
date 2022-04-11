a1dbbef67fb494f29d8ca613dd0284bc379743dc
chetan-cv <chetanverma99.cv@gmail.com>
2022-04-11 22:42:04 +0530

    updates

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import LogoGif from './components/LogoGif/logoGif';
-import HOME from './components/Home/home';
 import React from 'react';
 
 function App() {
   return (
     <div>
     <Router basename={process.env.PUBLIC_URL}>
-      <Route exact path="/" component={HOME} />      
+      <Route exact path="/" component={LogoGif} />      
     </Router>
     </div>
   );
